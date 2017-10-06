3c580941c1cec175fff59da126f012393a4d3f8f
kerennipo <keren.nipo@gmail.com>
2017-10-06 14:12:21 +0300

    Add about+ map with all locations

---
+++ DIneWithWebSite/Controllers/AboutController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using WebSite.Models;
+
+namespace DIneWithWebSite.Controllers
+{
+    public class AboutController : Controller
+    {
+        private BlogDBContext db = new BlogDBContext();
+
+        // GET: About
+        public ActionResult Index()
+        {
+            return View(db.Posts.ToList());
+        }
+
+        // GET: About/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Post post = db.Posts.Find(id);
+            if (post == null)
+            {
+                return HttpNotFound();
+            }
+            return View(post);
+        }
+
+        // GET: About/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: About/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,Title,Writer,WebSite,PublishDate,Content,Cuisine,Meals,RFeatures,Address,PhoneNumber,Image,Video,SundayOpening,SundayClosing,MondayOpening,MondayClosing,TuesdayOpening,TuesdayClosing,WednesdayOpening,WednesdayClosing,ThursdayOpening,ThursdayClosing,FridayOpening,FridayClosing,SaturdayOpening,SaturdayClosing,Latitude,Longitude")] Post post)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Posts.Add(post);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(post);
+        }
+
+        // GET: About/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Post post = db.Posts.Find(id);
+            if (post == null)
+            {
+                return HttpNotFound();
+            }
+            return View(post);
+        }
+
+        // POST: About/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,Title,Writer,WebSite,PublishDate,Content,Cuisine,Meals,RFeatures,Address,PhoneNumber,Image,Video,SundayOpening,SundayClosing,MondayOpening,MondayClosing,TuesdayOpening,TuesdayClosing,WednesdayOpening,WednesdayClosing,ThursdayOpening,ThursdayClosing,FridayOpening,FridayClosing,SaturdayOpening,SaturdayClosing,Latitude,Longitude")] Post post)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(post).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(post);
+        }
+
+        // GET: About/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Post post = db.Posts.Find(id);
+            if (post == null)
+            {
+                return HttpNotFound();
+            }
+            return View(post);
+        }
+
+        // POST: About/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Post post = db.Posts.Find(id);
+            db.Posts.Remove(post);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
